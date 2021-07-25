322af3534e81707d00008001735fbbd8816af1ee
Poorvi1234321 <74653455+Poorvi1234321@users.noreply.github.com>
2021-07-26 02:30:11 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -19,10 +19,11 @@ class Block{
       image(this.image,pos.x,pos.y,this.width,this.height);
      }
      else{
+      World.remove(world,this.body);
       push();
       this.visibility = this.visibility - 10;
       tint(255,this.visibility);
-      World.remove(world,this.body);
+      image(this.image,this.body.position.x,this.body.position.y,this.width,this.height);
       pop();
     }
 
